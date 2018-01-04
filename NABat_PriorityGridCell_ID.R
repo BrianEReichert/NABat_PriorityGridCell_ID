f7da84de25490577a9ad2b7357e2276f7e51e79f
BrianEReichert <breichert@usgs.gov>
2018-01-04 10:09:30 -0700

    Updated this code to add new column to the spatial object 'X' indicating which grid cells are within the top 5% for the entire state (CA in this example).

--- NABat_PriorityGridCell_ID.R
+++ NABat_PriorityGridCell_ID.R
@@ -16,6 +16,21 @@ names(X@data)
 summary(X)
 projection(X)
 #shp<-X
+###  Identify priority grid cells for all of California based on lowest GRTS_ID 
+(tot.grts.CA=length(unique(X@data$GRTS_ID))) # 4506 
+top5perc.CAgrts=sort(unique(X@data$GRTS_ID))[1:round(tot.grts.CA*.05)] # or top 5% GRTS priority cells in X and X2
+
+###  Create, map and write shapefile of top priority grid cells only 
+top5perc_CAgrts<-X[X@data$GRTS_ID %in% top5perc.CAgrts,]
+plot(X)
+plot(top5perc_CAgrts,add=TRUE, col = "blue")
+layer.name<-c("top5perc_CA_grts")
+writeOGR(obj=top5perc_CAgrts, dsn=folder,
+         layer=layer.name,driver="ESRI Shapefile")
+
+####  Add column to attribute table of X indicating whether grid cell is in the top 5% of cells (lowest GRTS_ID) for the entire state
+X@data$CA_top5perc<-0
+X@data$CA_top5perc[X@data$GRTS_ID %in% top5perc.CAgrts]<-1
 
 #extra code if you were to include a neighboring state (e.g., Nevada)
 #folder=getwd()
@@ -69,6 +84,7 @@ projection(X)
 ogrListLayers("UC_NRS.gdb")
 Y <- rgdal::readOGR(dsn="UC_NRS.gdb", layer="NRS_Boundaries")
 
+#### Check out the file attributes
 shp<-Y
 shp@data
 #view class
@@ -83,9 +99,9 @@ crs(shp)
 shp
 plot(shp)
 
-############ Make sure that projections match between objects
+############ Make sure that projections match between X and Y spatial objects
 Y <- spTransform(Y, crs(X))
-############ extract results from spatial join as data frame
+############ extract results from spatial join and store as a data frame
 join=over(x=X, y=Y)
 str(join)
 class(join)
@@ -104,9 +120,11 @@ plot(Z, add=TRUE)
 top25.grts=sort(unique(Z@data$GRTS_ID)) [1:25]  #[1:round(tot.grts*.05)] # or top 5% GRTS priority cells in X and X2
 top25_grts<-Z[Z@data$GRTS_ID %in% top25.grts,]
 
+
 plot(Y, col="green")
 plot(Z, add=TRUE)
 plot(top25_grts,add=TRUE, col = "red")
+plot(top25_grts,add=TRUE, col = top25_grts@data$CA_top5perc)
 
 ### Write shapefile of top priority grid cells only 
 layer.name<-c("top25_grts_UCNRS")
@@ -135,6 +153,7 @@ plot(Y, col ="green")
 col<-X2@data$top25
 col[which(col==1)]<-"red"
 col[which(join$Name!="<NA>"&col!="red")]<-"green"
+col[which(X2@data$CA_top5perc==1&X2@data$top25==1)]<-"blue"
 plot(X2,col= col,add=TRUE)
 
 ### Write shapefile with all grid cells with top priority cells indicated in data column "top25" 
